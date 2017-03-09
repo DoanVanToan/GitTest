8742ed692857a7daede488773f4760c52b56b1d3
ToanDoan <toandoan.coder@gmail.c>
2017-03-09 15:23:50 +0700

    init project
    fix_comment
    
    fix
    
    Fix hardcode

---
+++ app/src/main/java/com/toandoan/gittest/ui/activity/MainActivity.java
@@ -0,0 +1,14 @@
+package com.toandoan.gittest.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import kekiru.toandoan.com.gittest.R;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
